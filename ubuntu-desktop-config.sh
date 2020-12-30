7889f53297c1536e7353721f79d63e1730d84516
Simon Fraipont <simon.fraipont@gmail.com>
2020-12-30 12:38:22 +0100

    fix ubuntu script

--- ubuntu-desktop-config.sh
+++ ubuntu-desktop-config.sh
@@ -5,16 +5,16 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source ['<Primary>space']
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Primary>space']"
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 
 # Lower keyboard repeat delay to 200 (faster repeat when holding down a key) and lower keyboard repeat interval
 gsettings set org.gnome.desktop.peripherals.keyboard delay 200
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval uint32 25
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25
 
 # Various night light settings
-gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature uint32 3228
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 3228
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 10.0
