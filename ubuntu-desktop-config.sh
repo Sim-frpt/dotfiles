3e5c46915bbf8d13b7c4ef4b61725cd1de73e2e6
Simon Fraipont <simon.fraipont@gmail.com>
2020-12-30 15:38:16 +0100

    refactor ubuntu config idk if better

--- ubuntu-desktop-config.sh
+++ ubuntu-desktop-config.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-# This is a set of preferences for the Ubuntu desktop env
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
-gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
-gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Primary>space']"
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Primary><Shift>space']"
+function change_setting() {
+  schema=$1
+  key=$2
+  value=$3
+  writable=$(gsettings writable $schema $key)
 
-gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
-gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+  if [ $? -eq 0 ] && [ $writable == true ]; then
+    echo "Changing '${schema}' '${key}' to '${value}'"
+    gsettings set $schema $key $value
+  else
+    echo "ERROR: could not set ${schema} ${key} to ${value}"
+  fi
+}
+
+# This is a set of preferences for the Ubuntu desktop env
+change_setting org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+change_setting org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
+change_setting org.gnome.desktop.interface gtk-theme 'Yaru-dark'
+change_setting org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+change_setting org.gnome.desktop.wm.keybindings switch-input-source "['<Primary>space']"
+change_setting org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Primary><Shift>space']"
+change_setting org.gnome.desktop.peripherals.mouse natural-scroll false
+change_setting org.gnome.desktop.peripherals.touchpad natural-scroll false
 
 # Lower keyboard repeat delay to 200 (faster repeat when holding down a key) and lower keyboard repeat interval
-gsettings set org.gnome.desktop.peripherals.keyboard delay 200
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25
+change_setting org.gnome.desktop.peripherals.keyboard delay 200
+change_setting org.gnome.desktop.peripherals.keyboard repeat-interval 25
 
 # Various night light settings
-gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 3228
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 10.0
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 17.0
+change_setting org.gnome.settings-daemon.plugins.color night-light-temperature 3228
+change_setting org.gnome.settings-daemon.plugins.color night-light-enabled true
+change_setting org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
+change_setting org.gnome.settings-daemon.plugins.color night-light-schedule-to 10.0
+change_setting org.gnome.settings-daemon.plugins.color night-light-schedule-from 17.0
