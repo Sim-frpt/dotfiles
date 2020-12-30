0a38954496a82623b86b9403f8881874a47e5a5a
Simon Fraipont <simon.fraipont@gmail.com>
2020-12-30 12:31:37 +0100

    add option to ubuntu desktop preferences script

--- ubuntu-desktop-config.sh
+++ ubuntu-desktop-config.sh
@@ -4,9 +4,10 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
-gsettings set org.gnome.desktop.input-sources xkb-options ['caps:escape']
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source ['<Primary>space']
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 
 # Lower keyboard repeat delay to 200 (faster repeat when holding down a key) and lower keyboard repeat interval
 gsettings set org.gnome.desktop.peripherals.keyboard delay 200
