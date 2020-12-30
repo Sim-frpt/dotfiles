543be9e1ca1221c2f6ce05fe1be65cf8c90e81f6
Simon Fraipont <simon.fraipont@gmail.com>
2020-12-30 11:47:42 +0100

    add ubuntu desktop config script

---
+++ ubuntu-desktop-config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This is a set of preferences for the Ubuntu desktop env
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
+gsettings set org.gnome.desktop.input-sources xkb-options ['caps:escape']
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source ['<Primary>space']
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
+
+# Lower keyboard repeat delay to 200 (faster repeat when holding down a key) and lower keyboard repeat interval
+gsettings set org.gnome.desktop.peripherals.keyboard delay 200
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval uint32 25
+
+# Various night light settings
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature uint32 3228
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 10.0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 17.0
+
+
+
