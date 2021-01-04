43e1f0ec68809121ec937b0e54c968a649e74707
Simon Fraipont <simon.fraipont@gmail.com>
2021-01-04 15:32:25 +0100

    add ruby env as rbenv plugin in rbenv install

--- scripts/programs/rbenv.sh
+++ scripts/programs/rbenv.sh
@@ -12,3 +12,6 @@ git clone https://github.com/rbenv/rbenv.git $rbenv_dir
 cd $rbenv_dir && src/configure && make -C src
 $rbenv_dir/bin/rbenv init
 
+# Install ruby-build as an rbenv plugin
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
