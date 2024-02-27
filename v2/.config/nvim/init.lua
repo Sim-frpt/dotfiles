dc67b249ec7f45fba685525cfa03ef4feaf5be83
Simon Fraipont <simon.fraipont@gmail.com>
2024-02-27 08:48:20 +0100

    add telescope and treesitter to neovim

--- v2/.config/nvim/init.lua
+++ v2/.config/nvim/init.lua
@@ -4,22 +4,28 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
--- Declare the list of plugins.
 
--- " Color theme
 Plug('rebelot/kanagawa.nvim')
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x' })
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
 
 vim.call('plug#end')
 
--- " VIM OPTIONS
+-- VIM OPTIONS
 
--- " Color scheme
+-- Color scheme
 vim.cmd.colorscheme('kanagawa')
 
--- " Set line numbers and make them relative to cursor position
+-- Set line numbers and make them relative to cursor position
 vim.o.number = true
 vim.o.relativenumber = true
 
--- " Briefly jump to the matching brace/paren/bracket when inserting text
-
+-- Briefly jump to the matching brace/paren/bracket when inserting text
 vim.o.showmatch = true
+
+-- Copy from vim inside system clipboard
+vim.o.clipboard = "unnamedplus"
+
+
+
--- v2/.config/nvim/lua/sim/remaps.lua
+++ v2/.config/nvim/lua/sim/remaps.lua
@@ -5,11 +5,11 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- " Center the view when searching something
+-- Center the view when searching something
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 
--- " This allows to keep the current clipboard item active when replacing text
+-- This allows to keep the current clipboard item active when replacing text
 vim.keymap.set("x", "<leader>p", "\"_dp")
 
 
